9aead480899b50781cb6e5fb5c6a7d5d85fff629
Arachne <66822642+Arachneee@users.noreply.github.com>
2026-01-04 00:35:34 +0900

    build: 의존성 및 빌드 설정 업데이트

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,14 @@
 plugins {
     kotlin("jvm") version "2.1.21"
+    kotlin("plugin.serialization") version "2.1.21"
+    application
 }
 
-group = "com"
+application {
+    mainClass.set("com.evoshot.EvoShotApplicationKt")
+}
+
+group = "com.evoshot"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -11,12 +17,22 @@ repositories {
 
 dependencies {
     implementation("io.netty:netty-all:4.1.117.Final")
+    
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    
+    implementation("org.slf4j:slf4j-api:2.0.16")
+    implementation("ch.qos.logback:logback-classic:1.5.12")
+    
     testImplementation(kotlin("test"))
+    testImplementation("org.assertj:assertj-core:3.26.3")
+    testImplementation("io.mockk:mockk:1.13.13")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
 }
