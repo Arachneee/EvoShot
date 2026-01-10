e720a31c9f2db1199251cb6c41382d4f5a6e6e8c
Arachne <66822642+Arachneee@users.noreply.github.com>
2026-01-10 15:56:10 +0900

    add jbox2d

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,13 +18,14 @@ repositories {
 
 dependencies {
     implementation("io.netty:netty-all:4.1.117.Final")
-    
+    implementation("org.jbox2d:jbox2d-library:2.2.1.1")
+
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
-    
+
     implementation("org.slf4j:slf4j-api:2.0.16")
     implementation("ch.qos.logback:logback-classic:1.5.12")
-    
+
     testImplementation(kotlin("test"))
     testImplementation("org.assertj:assertj-core:3.26.3")
     testImplementation("io.mockk:mockk:1.13.13")
